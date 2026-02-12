965498d0b8fb2e7607f88fac42a6c3cffcd5927c
Paulo Castellano <paulofcastellano@gmail.com>
2026-02-12 11:13:37 -0300

    fix(ui): forward $attrs in Reka UI portal wrapper components
    Adds inheritAttrs: false and manual $attrs forwarding to 6 components
    that render portal fragments, fixing Vue warnings for data-testid and
    other HTML attributes.

--- src/renderer/components/ui/dropdown-menu/DropdownMenuContent.vue
+++ src/renderer/components/ui/dropdown-menu/DropdownMenuContent.vue
@@ -3,6 +3,8 @@ import type { HTMLAttributes } from 'vue'
 import { DropdownMenuContent, type DropdownMenuContentEmits, type DropdownMenuContentProps, DropdownMenuPortal, useForwardPropsEmits } from 'reka-ui'
 import { cn } from '@/lib/utils'
 
+defineOptions({ inheritAttrs: false })
+
 const props = withDefaults(defineProps<DropdownMenuContentProps & { class?: HTMLAttributes['class'] }>(), {
   sideOffset: 4,
 })
@@ -15,7 +17,7 @@ const forwarded = useForwardPropsEmits(props, emits)
 <template>
   <DropdownMenuPortal>
     <DropdownMenuContent
-      v-bind="forwarded"
+      v-bind="{ ...forwarded, ...$attrs }"
       :class="cn('z-50 min-w-32 overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2', props.class)"
     >
       <slot />
